1130c30f21623b4b72fabd500e8655becb155d71
amir <ladsgroup@gmail.com>
2015-03-18 01:53:24 +0330

    PEP8
    Part II, last part

--- ores/wsgi/errors.py
+++ ores/wsgi/errors.py
@@ -4,5 +4,6 @@ from flask import jsonify
 def error(status, code, message):
     return jsonify({'error': {'code': code, 'message': message}}), status
 
+
 def bad_request(message):
     return error(400, 'request invalid', message)
