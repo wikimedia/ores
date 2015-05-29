01213f15f263cf3e2b96eb29491e8b7db5a73788
YuviPanda <yuvipanda@gmail.com>
2015-05-29 12:15:22 +0100

    Fix PEP8 issues in feature_lists

--- ores/feature_lists/azwiki.py
+++ ores/feature_lists/azwiki.py
@@ -1,9 +1,4 @@
-from revscoring.features import (
-    diff, page, parent_revision, previous_user_revision, revision, user
-)
-from revscoring.features.modifiers import log
 from . import generic
 
 damaging = generic.damaging
 good_faith = generic.good_faith
-
