8704d84b1f488959c2cb74f60dcc8cf54b20caed
halfak <aaron.halfaker@gmail.com>
2015-07-08 18:29:45 -0500

    Adds socketIO-client to requirements.txt

--- ores/ores.py
+++ ores/ores.py
@@ -3,11 +3,8 @@ This script provides access to a set of utilities for ORES
 
 * label_reverted -- Labels reverted revisions
 * dev_server -- Starts a development WSGI server
-<<<<<<< HEAD
 * celery_worker -- Starts a "ScoreProcessor" celery worker
-=======
 * precached -- Starts a daemon that requests scores for revisions as they happen
->>>>>>> Updates the ores utility to list the precached utility.
 
 Usage:
     ores (-h | --help)
