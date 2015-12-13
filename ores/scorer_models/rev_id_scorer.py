670740fb9296b3139325e0da83fecd2284cfaeae
halfak <aaron.halfaker@gmail.com>
2015-12-13 09:56:39 -0600

    Updates rev_id_scorer to reverse digits and increments requirements for using the new offline extractor.

--- ores/scorer_models/rev_id_scorer.py
+++ ores/scorer_models/rev_id_scorer.py
@@ -3,12 +3,16 @@ from revscoring.datasources import revision
 from revscoring.features import Feature
 
 
-def process_last_two_in_rev_id(rev_id):
-    return int(str(rev_id)[-2:])
+def process_reversed_last_two_in_rev_id(rev_id):
+    last_two = str(rev_id)[-2:]
+    if len(last_two) == 1:
+        return int(last_two + "0")
+    else:
+        return int("".join(reversed(last_two)))
 
-last_two_in_rev_id = Feature(
-    "revision.last_two_in_rev_id",
-    process_last_two_in_rev_id,
+reversed_last_two_in_rev_id = Feature(
+    "revision.reversed_last_two_in_rev_id",
+    process_reversed_last_two_in_rev_id,
     returns=int,
     depends_on=[revision.id]
 )
@@ -17,10 +21,12 @@ last_two_in_rev_id = Feature(
 class RevIdScorer(ScorerModel):
     """
     Implements a basic, testing scorer that predicts whether a revision ID's
-    last two digits are greater than 50.
+    reversed last two digits are greater than 50.
+
+    E.g. 974623 = 32 and 23754929 = 92
     """
     def __init__(self, version=None):
-        super().__init__([last_two_in_rev_id], version=version)
+        super().__init__([reversed_last_two_in_rev_id], version=version)
 
     def score(self, feature_values):
         probability = feature_values[0] / 100
