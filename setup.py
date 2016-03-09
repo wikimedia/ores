25453b79a11a4897a60fabb245ddc6726ee18c9c
halfak <aaron.halfaker@gmail.com>
2016-03-09 14:19:48 -0600

    Sets upper bound on revscoring to 0.7.x and increments version to 0.5.8

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def requirements(fname):
 
 setup(
     name="ores",
-    version="0.5.7", # Update in ores/__init__.py too.
+    version="0.5.8",  # Update in ores/__init__.py too.
     author="Aaron Halfaker",
     author_email="ahalfaker@wikimedia.org",
     description=("A webserver for hosting scoring services."),
