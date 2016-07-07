45420e89ceb60b9aa8b090ab21176373222bcd36
halfak <aaron.halfaker@gmail.com>
2016-07-07 09:51:41 -0500

    score_processor --> scoring_system and more responsibility in scoring_context.

--- ores/score_processors/tests/test_score_processor.py
+++
@@ -1,6 +0,0 @@
-from ..score_processor import ScoreProcessor, SimpleScoreProcessor
-
-
-def test_score_process():
-    ssp = SimpleScoreProcessor({})
-    assert isinstance(ssp, ScoreProcessor)
---
+++ ores/scoring_systems/process_pool.py
@@ -0,0 +1,58 @@
+import logging
+from concurrent import futures as cfutures
+import revscoring.errors
+
+from .scoring_system import ScoringSystem
+from ..errors import TimeoutError
+
+logger = logging.getLogger(__name__)
+
+
+class ProcessPool(ScoringSystem):
+
+    def __init__(self, *args, workers=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.workers = int(workers) if workers is not None else None
+
+    def _process_missing_scores(self, context_name, missing_model_set_revs,
+                                root_caches, injection_caches,
+                                include_features, inprogress_results=None):
+        rev_scores = {}
+        errors = {}
+
+        futures = {}
+        with cfutures.ProcessPoolExecutor(max_workers=self.workers) as executor:
+            for missing_models, rev_ids in missing_model_set_revs.items():
+                for rev_id in rev_ids:
+                    if rev_id not in root_caches:
+                        continue
+                    root_cache = root_caches[rev_id]
+                    injection_cache = injection_caches.get(rev_id) \
+                                      if injection_caches is not None else None
+                    future = executor.submit(
+                        self._process_score_map,
+                        context_name, missing_models, rev_id, root_cache,
+                        injection_cache, include_features)
+                    futures[rev_id] = future
+
+            for rev_id, future in futures.items():
+                try:
+                    rev_scores[rev_id] = future.result(timeout=self.timeout)
+                except cfutures.TimeoutError:
+                    errors[rev_id] = TimeoutError(
+                        "Timed out after {0} seconds.".format(self.timeout))
+                except Exception as error:
+                    errors[rev_id] = error
+
+        return rev_scores, errors
+
+    @classmethod
+    def from_config(cls, config, name, section_key="scoring_systems"):
+        logger.info("Loading ProcessPool '{0}' from config.".format(name))
+        section = config[section_key][name]
+
+        kwargs = cls._kwargs_from_config(
+            config, name, section_key=section_key)
+        workers = section.get('workers')
+
+        return cls(workers=workers, **kwargs)
