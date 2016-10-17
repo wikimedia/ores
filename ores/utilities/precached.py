bc993ec1b5c7721f8427562f4570367a35bbf68f
halfak <aaron.halfaker@gmail.com>
2016-10-17 15:05:57 -0500

    Fixes memory leak in precached.

--- ores/utilities/precached.py
+++ ores/utilities/precached.py
@@ -33,12 +33,13 @@ import requests
 import socketIO_client
 import yamlconf
 
-from .watchdog import notify_socket, watchdog_ping
 from ..metrics_collectors import MetricsCollector, Null
+from .watchdog import notify_socket, watchdog_ping
 
 logger = logging.getLogger(__name__)
 
 AVAILABLE_EVENTS = {'edit', 'nonbot_edit'}
+MAX_WORKERS = 50
 
 
 def main(argv=None):
@@ -84,6 +85,23 @@ def run(stream_url, ores_url, metrics_collector, config, delay, notify, verbose)
 
     if not notify:
         logger.info('Not being ran as a service, watchdog disabled')
+
+    score_on = build_score_on(config)
+    RCNamespace = build_RCNamespace(
+        stream_url, score_on, ores_url, MAX_WORKERS, metrics_collector, delay,
+        notify)
+
+    socketIO = socketIO_client.SocketIO(stream_url, 80)
+    socketIO.define(RCNamespace, '/rc')
+
+    try:
+        socketIO.wait(seconds=sys.maxsize)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected.  Shutting down.")
+        socketIO.disconnect()
+
+
+def build_score_on(config):
     # Build a mapping of wikis and models from the configuration
     score_on = defaultdict(list)
     ss_name = config['ores']['scoring_system']
@@ -102,77 +120,88 @@ def run(stream_url, ores_url, metrics_collector, config, delay, notify, verbose)
                 logger.debug("Setting up precaching for {0} in {1} on {2}"
                              .format(model, context, event))
 
-    def score_revision(context_name, model_names, rev_id):
-        if not model_names:
-            return
-        url = ores_url + "/v2/scores/" + context_name + "/" + \
-              "?models=" + "|".join(model_names) + \
-              "&revids=" + str(rev_id) + \
-              "&precache=true"
-        try:
-            time.sleep(delay)
-            start = time.time()
-            response = requests.get(url, timeout=20, verify=True)
-            if response.status_code == 200:
-                first_model_doc = \
-                    response.json()['scores'][context_name][model_names[0]]
-                if 'error' not in first_model_doc['scores'][str(rev_id)]:
-                    metrics_collector.precache_score(
-                        context_name, model_names, time.time() - start)
+    return score_on
+
+
+def build_RCNamespace(stream_url, score_on, ores_url, max_workers,
+                       metrics_collector, delay, notify):
+
+    class _RCNamespace(socketIO_client.BaseNamespace):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            print("RecentChanges.__init__", args, kwargs)
+            self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+
+        def on_change(self, change):
+            if change['type'] in ('new', 'edit'):
+                wikidb = change['wiki']
+                rev_id = change['revision']['new']
+                models = score_on.get(('edit', wikidb), [])
+                self.score_revision(wikidb, models, rev_id)
+
+                if not change.get('bot'):
+                    models = score_on.get(('nonbot_edit', wikidb), [])
+                    self.score_revision(wikidb, models, rev_id)
+
+        def on_connect(self):
+            logger.info("Connecting socketIO client to {0}."
+                        .format(stream_url))
+
+            # TODO: This should be limited to the wikis we actually care about
+            # but we identify a wiki by it's dbname and this pattern matching
+            # uses a domain name.
+            self.emit('subscribe', '*')  # Subscribes to all wikis
+
+        def on_disconnect(self):
+            self.executor.shutdown(wait=False)
+
+        def score_revision(self, context_name, model_names, rev_id):
+            self.executor.submit(
+                self._score_revision, context_name, model_names, rev_id)
+
+        def _score_revision(self, context_name, model_names, rev_id):
+            if not model_names:
+                return
+            url = ores_url + "/v2/scores/" + context_name + "/" + \
+                  "?models=" + "|".join(model_names) + \
+                  "&revids=" + str(rev_id) + \
+                  "&precache=true"
+            try:
+                time.sleep(delay)
+                start = time.time()
+                response = requests.get(url, timeout=20, verify=True)
+                if response.status_code == 200:
+                    first_model_doc = \
+                        response.json()['scores'][context_name][model_names[0]]
+                    if 'error' not in first_model_doc['scores'][str(rev_id)]:
+                        metrics_collector.precache_score(
+                            context_name, model_names, time.time() - start)
+                    else:
+                        metrics_collector.precache_scoring_error(
+                            context_name, model_names, response.status_code,
+                            time.time() - start)
+                        logger.warning(
+                            "Request to {0} failed with {1} response: \n\t{2}"
+                            .format(url, response.status_code,
+                                    first_model_doc['scores'][str(rev_id)]))
+
                 else:
                     metrics_collector.precache_scoring_error(
                         context_name, model_names, response.status_code,
                         time.time() - start)
                     logger.warning(
                         "Request to {0} failed with {1} response: \n\t{2}"
-                        .format(url, response.status_code,
-                                first_model_doc['scores'][str(rev_id)]))
-
-            else:
+                        .format(url, response.status_code, response.content[:500]))
+                if notify:
+                    watchdog_ping(*notify)
+            except requests.exceptions.ReadTimeout:
                 metrics_collector.precache_scoring_error(
-                    context_name, model_names, response.status_code,
+                    context_name, model_names, 504,
                     time.time() - start)
                 logger.warning(
-                    "Request to {0} failed with {1} response: \n\t{2}"
-                    .format(url, response.status_code, response.content[:500]))
-            if notify:
-                watchdog_ping(*notify)
-        except requests.exceptions.ReadTimeout:
-            metrics_collector.precache_scoring_error(
-                context_name, model_names, 504,
-                time.time() - start)
-            logger.warning(
-                "Request to {0} timed out in {1} seconds"
-                .format(url, round(time.time() - start, 3)))
-        except Exception as e:
-            logger.error(str(type(e)) + ": " + str(e))
+                    "Request to {0} timed out in {1} seconds"
+                    .format(url, round(time.time() - start, 3)))
+            except Exception as e:
+                logger.error(str(type(e)) + ": " + str(e))
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        class WikiNamespace(socketIO_client.BaseNamespace):
-            def on_change(self, change):
-                if change['type'] in ('new', 'edit'):
-                    wikidb = change['wiki']
-                    rev_id = change['revision']['new']
-                    models = score_on[('edit', wikidb)]
-                    executor.submit(score_revision, wikidb, models, rev_id)
-
-                    if not change.get('bot'):
-                        models = score_on[('nonbot_edit', wikidb)]
-                        executor.submit(score_revision, wikidb, models, rev_id)
-
-            def on_connect(self):
-                logger.info("Connecting socketIO client to {0}."
-                            .format(stream_url))
-
-                # TODO: This should be limited to the wikis we actually care about
-                # but we identify a wiki by it's dbname and this pattern matching
-                # uses a domain name.
-                self.emit('subscribe', '*')  # Subscribes to all wikis
-
-        socketIO = socketIO_client.SocketIO(stream_url, 80)
-        socketIO.define(WikiNamespace, '/rc')
-
-        try:
-            socketIO.wait(seconds=sys.maxsize)
-        except KeyboardInterrupt:
-            print("Keyboard interrupt detected.  Shutting down.")
+    return _RCNamespace
