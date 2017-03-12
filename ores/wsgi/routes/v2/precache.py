069d94cd55df44c0568e5669a0af55d87c7f3601
Amir Sarabadani <ladsgroup@gmail.com>
2017-03-12 17:46:09 +0330

    MAke precache endpoint understand Wikimedia's CP schema

--- ores/wsgi/routes/v2/precache.py
+++ ores/wsgi/routes/v2/precache.py
@@ -1,11 +1,13 @@
+import json
 import traceback
 
 from flask import request
 from flask.ext.jsonpify import jsonify
 
+from urllib.parse import unquote
+
 from ... import preprocessors, responses
 from .... import errors
-from ...util import ParamError, read_bar_split_param
 
 
 def configure(config, bp, scoring_system):
@@ -14,28 +16,29 @@ def configure(config, bp, scoring_system):
     @preprocessors.nocache
     @preprocessors.minifiable
     def precache_v2():
-        if 'wiki' not in request.args or 'revids' not in request.args:
-            return responses.bad_request("'wiki' and 'revids' arguments are required")
-        context = request.args.get('wiki')
+        try:
+            data = json.loads(unquote(request.args.get('data', '{}')).strip())
+        except json.JSONDecodeError:
+            return responses.bad_request("Can not parse data argument")
+        if 'database' not in data or 'rev_id' not in data:
+            return responses.bad_request("'database' and 'rev_id' arguments are required")
+        context = data['database']
         # Check to see if we have the context available in our score_processor
         if context not in scoring_system:
             return responses.not_found("No scorers available for {0}"
                                        .format(context))
 
         # Read the rev_ids
-        try:
-            rev_ids = set(read_bar_split_param(request, "revids", type=int))
-        except ParamError as e:
-            return responses.bad_request(str(e))
-        if not rev_ids:
-            return responses.bad_request(str(e))
+        rev_id = data['rev_id']
 
         response_doc = {context: {}}
         precache_config = config['scoring_contexts'].get(context, {}).get('precache', {})
-        if 'bot' in request.args and precache_config:
+        performer_user_groups = data.get('performer', {}).get('user_groups', [])
+        if precache_config and performer_user_groups:
             models = []
             for model in precache_config:
-                if 'nonbot_edit' not in precache_config[model]['on']:
+                if ('nonbot_edit' not in precache_config[model]['on'] and
+                        'bot' not in performer_user_groups):
                     models.append(model)
         else:
             models = precache_config.keys()
@@ -46,7 +49,7 @@ def configure(config, bp, scoring_system):
 
         try:
             scores_doc = scoring_system.score(
-                context, models, rev_ids, precache=True,
+                context, models, [rev_id], precache=True,
                 include_model_info=None)
         except errors.ScoreProcessorOverloaded:
             return responses.server_overloaded()
@@ -55,13 +58,12 @@ def configure(config, bp, scoring_system):
 
         for model_name in models:
             response_doc[context][model_name]['scores'] = {}
-            for rev_id in rev_ids:
-                if 'error' in scores_doc['scores'][rev_id]:
-                    response_doc[context][model_name]['scores'][rev_id] = \
-                        scores_doc['scores'][rev_id]
-                else:
-                    response_doc[context][model_name]['scores'][rev_id] = \
-                        scores_doc['scores'][rev_id][model_name]['score']
+            if 'error' in scores_doc['scores'][rev_id]:
+                response_doc[context][model_name]['scores'][rev_id] = \
+                    scores_doc['scores'][rev_id]
+            else:
+                response_doc[context][model_name]['scores'][rev_id] = \
+                    scores_doc['scores'][rev_id][model_name]['score']
 
         return jsonify({'scores': response_doc})
 
