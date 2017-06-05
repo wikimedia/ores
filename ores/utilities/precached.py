5755756b476780159955574ad72817ceb243e941
halfak <aaron.halfaker@gmail.com>
2017-06-05 17:54:57 -0500

    Simplified precached, use /precache endpoint.

--- ores/utilities/precached.py
+++ ores/utilities/precached.py
@@ -1,14 +1,14 @@
 """
-Runs a pre-caching server against an ORES instance by listening to an RCStream
-and submitting requests for scores to the web interface for each revision as it
-happens.
+Runs a pre-caching server against an ORES instance by listening to an
+EventStream and submitting requests for scores to the web interface for each
+revision as it happens.
 
 If ran by systemd, it uses watchdog to stay alive.
 
 :Usage:
     precached -h | --help
     precached <stream-url> <ores-url> [--config=<path>] [--delay=<secs>]
-                                      [--verbose]
+                                      [--debug]
 
 :Options:
     -h --help        Prints this documentation
@@ -18,32 +18,40 @@ If ran by systemd, it uses watchdog to stay alive.
                      request is sent to ORES. [default: 0]
     --config=<path>  The path to a yaml config file
                      [default: config]
-    --verbose        Print debugging information
+    --debug          Print debugging information
 """
-import json
 import glob
+import json
 import logging
 import os
-import sys
 import time
-from collections import defaultdict
-from sseclient import SSEClient as EventSource
+from concurrent.futures import ThreadPoolExecutor
 
 import docopt
 import requests
 import yamlconf
+from sseclient import SSEClient
 
 from ..metrics_collectors import MetricsCollector, Null
 from .watchdog import notify_socket, watchdog_ping
 
 logger = logging.getLogger(__name__)
 
-AVAILABLE_EVENTS = {'edit', 'nonbot_edit'}
 MAX_WORKERS = 50
 
 
 def main(argv=None):
     args = docopt.docopt(__doc__, argv=argv)
+
+    logging.basicConfig(
+        level=logging.INFO if not args['--debug'] else logging.DEBUG,
+        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
+    )
+    # Requests is loud.  Be quiet requests.
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    requests.packages.urllib3.disable_warnings()
+    # If we're using logging for metrics collection, show it.
+    logging.getLogger("ores.metrics_collectors").setLevel(logging.DEBUG)
 
     stream_url = args['<stream-url>']
     ores_url = args['<ores-url>']
@@ -53,6 +61,8 @@ def main(argv=None):
     delay = float(args['--delay'])
     verbose = bool(args['--verbose'])
     notify = notify_socket()
+    if not notify:
+        logger.info('Not being ran as a service, watchdog disabled')
     ss_name = config['ores']['scoring_system']
     if 'metrics_collector' in config['scoring_systems'][ss_name]:
         metrics_collector = MetricsCollector.from_config(
@@ -66,120 +76,32 @@ def main(argv=None):
 
 def run(stream_url, ores_url, metrics_collector, config, delay, notify, verbose):
 
-    if verbose:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.INFO
+    # What to do in case of a change
+    def precache_a_change(change):
+        if delay:
+            time.sleep(delay)
+        if notify:
+            watchdog_ping(*notify)
+        start = time.time()
+        response = requests.post(
+            ores_url + "/v3/precache/", data=json.dumps(change))
 
-    logging.basicConfig(
-        level=log_level,
-        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
-    )
+        if response.status == 200:
+            logger.info("Scored {0} in {1} seconds."
+                        .format(json.dumps(change), round(time.time() - start, 3)))
+        elif response.status == 204:
+            logger.debug("Nothing to do for {0}".format(json.dumps(change)))
+        else:
+            logger.error("Scoring {0} and got an error in response:\n{1}"
+                         .format(json.dumps(change), response.content))
 
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    requests.packages.urllib3.disable_warnings()
-    # If we're using logging for metrics collection, show it.
-    logging.getLogger("ores.metrics_collectors").setLevel(logging.DEBUG)
+    # Execute changes!
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        for event in SSEClient(stream_url):
+            if event.event == 'message':
+                try:
+                    change = json.loads(event.data)
+                except ValueError:
+                    pass
 
-    if not notify:
-        logger.info('Not being ran as a service, watchdog disabled')
-
-    score_on = build_score_on(config)
-    RCNamespace = build_RCNamespace(
-        stream_url, score_on, ores_url, MAX_WORKERS, metrics_collector, delay,
-        notify)
-
-    for event in EventSource(stream_url):
-        if event.event == 'message':
-            try:
-                change = json.loads(event.data)
-            except ValueError:
-                pass
-            else:
-                RCNamespace.on_change(change)
-
-
-def build_score_on(config):
-    # Build a mapping of wikis and models from the configuration
-    score_on = defaultdict(list)
-    ss_name = config['ores']['scoring_system']
-    for context in config['scoring_systems'][ss_name]['scoring_contexts']:
-        for model in config['scoring_contexts'][context].get('precache', []):
-            precached_config = \
-                config['scoring_contexts'][context]['precache'][model]
-
-            events = precached_config['on']
-            if len(set(events) - AVAILABLE_EVENTS) > 0:
-                logger.error("{0} events are not available"
-                             .format(set(events) - AVAILABLE_EVENTS))
-                sys.exit(1)
-            for event in precached_config['on']:
-                score_on[(event, context)].append(model)
-                logger.debug("Setting up precaching for {0} in {1} on {2}"
-                             .format(model, context, event))
-
-    return score_on
-
-
-def build_RCNamespace(stream_url, score_on, ores_url, max_workers,
-                       metrics_collector, delay, notify):
-
-    class _RCNamespace():
-
-        def on_change(self, change):
-            if change['type'] in ('new', 'edit'):
-                wikidb = change['wiki']
-                rev_id = change['revision']['new']
-                models = score_on.get(('edit', wikidb), [])
-                self._score_revision(wikidb, models, rev_id)
-
-                if not change.get('bot'):
-                    models = score_on.get(('nonbot_edit', wikidb), [])
-                    self._score_revision(wikidb, models, rev_id)
-
-        def _score_revision(self, context_name, model_names, rev_id):
-            if not model_names:
-                return
-            url = ores_url + "/v2/scores/" + context_name + "/" + \
-                  "?models=" + "|".join(model_names) + \
-                  "&revids=" + str(rev_id) + \
-                  "&precache=true"
-            try:
-                time.sleep(delay)
-                start = time.time()
-                response = requests.get(url, timeout=20, verify=True)
-                if response.status_code == 200:
-                    first_model_doc = \
-                        response.json()['scores'][context_name][model_names[0]]
-                    if 'error' not in first_model_doc['scores'][str(rev_id)]:
-                        metrics_collector.precache_score(
-                            context_name, model_names, time.time() - start)
-                    else:
-                        metrics_collector.precache_scoring_error(
-                            context_name, model_names, response.status_code,
-                            time.time() - start)
-                        logger.warning(
-                            "Request to {0} failed with {1} response: \n\t{2}"
-                            .format(url, response.status_code,
-                                    first_model_doc['scores'][str(rev_id)]))
-
-                else:
-                    metrics_collector.precache_scoring_error(
-                        context_name, model_names, response.status_code,
-                        time.time() - start)
-                    logger.warning(
-                        "Request to {0} failed with {1} response: \n\t{2}"
-                        .format(url, response.status_code, response.content[:500]))
-                if notify:
-                    watchdog_ping(*notify)
-            except requests.exceptions.ReadTimeout:
-                metrics_collector.precache_scoring_error(
-                    context_name, model_names, 504,
-                    time.time() - start)
-                logger.warning(
-                    "Request to {0} timed out in {1} seconds"
-                    .format(url, round(time.time() - start, 3)))
-            except Exception as e:
-                logger.error(str(type(e)) + ": " + str(e))
-
-    return _RCNamespace
+                executor.submit(precache_a_change, change)
