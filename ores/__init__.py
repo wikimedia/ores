e15ec865470855c8466d2bf181b4e4b8924bbeeb
Devin/Zppix <megadev44s.mail@gmail.com>
2017-11-30 17:11:24 -0600

    Fix #noqa usage

--- ores/__init__.py
+++ ores/__init__.py
@@ -1,9 +1,9 @@
-#noqa
 import sys
 import platform
-if sys.version_info <= (3, 0):
-    from pkg_resources import VersionConflict
-    raise VersionConflict("ORES requires Python '>=3' but your Python version is " + platform.python_version())
+
+if sys.version_info <= (3, 0): #noqa
+    from pkg_resources import VersionConflict #noqa
+    raise VersionConflict("ORES requires Python '>=3' but your Python version is " + platform.python_version()) #noqa
 
 
 from .about import (__author__, __author_email__, __description__, __name__,
