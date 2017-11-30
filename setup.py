403797b65c7bf6a5ea0ca6f3b823a9b4917f9fff
Devin/Zppix <megadev44s.mail@gmail.com>
2017-11-30 14:12:58 -0600

    T181693: Py3 hard requirement

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import os
 import re
-
+import sys
+import platform
 from setuptools import find_packages, setup
 
 about_path = os.path.join(os.path.dirname(__file__), "ores/about.py")
 exec(compile(open(about_path).read(), about_path, "exec"))
 
+if sys.version_info <= (3, 0):
+    print("ORES needs Python 3 to run properly. Your version is " + platform.python_version())
+    sys.exit(1)
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
