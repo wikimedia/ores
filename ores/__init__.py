dcf1e6343e98f3b8170a7b36488a215c9387a70a
Devin/Zppix <megadev44s.mail@gmail.com>
2017-11-30 14:19:09 -0600

    T181693: Py3 hard requirement (init.py)

--- ores/__init__.py
+++ ores/__init__.py
@@ -1,5 +1,10 @@
+import sys
+import platform
 from .about import (__author__, __author_email__, __description__, __name__,
                     __url__, __version__)
 
 __all__ = [__name__, __version__, __author__, __author_email__,
            __description__, __url__]
+if sys.version_info <= (3, 0):
+    from pkg_resources import VersionConflict
+    raise VersionConflict("ORES requires Python '>=3' but your Python version is " + platform.python_version())
