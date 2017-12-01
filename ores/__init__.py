2cfab4371d63fc514df056568a3f0dfbf70e3ca2
Adam Roses Wight <awight@wikimedia.org>
2017-12-01 10:27:58 -0500

    Tweak to satisfy flake8

--- ores/__init__.py
+++ ores/__init__.py
@@ -1,13 +1,13 @@
 import sys
 import platform
-
-if sys.version_info <= (3, 0):  # noqa
-    from pkg_resources import VersionConflict  # noqa
-    raise VersionConflict("ORES requires Python '>=3' but your Python version is " + platform.python_version())  # noqa
-
+from pkg_resources import VersionConflict
 
 from .about import (__author__, __author_email__, __description__, __name__,
-                    __url__, __version__)  # noqa
+                    __url__, __version__)
+
+if sys.version_info <= (3, 0):
+    raise VersionConflict("ORES requires Python '>=3' but your Python version is " + platform.python_version())
+
 
 __all__ = [__name__, __version__, __author__, __author_email__,
            __description__, __url__]
