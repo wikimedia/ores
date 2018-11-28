50904ec2d703af0e3f2049f240ef17ed33bf89f8
Amir Sarabadani <Ladsgroup@gmail.com>
2018-11-28 11:49:25 +0100

    Add RedisSentinel class for score cache
    Bug: T210579

--- ores/score_caches/__init__.py
+++ ores/score_caches/__init__.py
@@ -1,6 +1,6 @@
 from .score_cache import ScoreCache
 from .empty import Empty
 from .lru import LRU
-from .redis import Redis
+from .redis import Redis, RedisSentinel
 
-__all__ = [ScoreCache, Empty, LRU, Redis]
+__all__ = [ScoreCache, Empty, LRU, Redis, RedisSentinel]
--- ores/score_caches/redis.py
+++ ores/score_caches/redis.py
@@ -5,9 +5,12 @@ from hashlib import sha1
 from .score_cache import ScoreCache
 
 logger = logging.getLogger("ores.score_caches.redis")
+sentinel_logger = logging.getLogger('ores.score_caches.RedisSentinel')
 
 TTL = 60 * 60 * 24 * 365 * 16  # 16 years
 PREFIX = "ores"
+CLUSTER = 'orescache'
+SOCKET_TIMEOUT = 0.1
 
 
 class Redis(ScoreCache):
@@ -81,3 +84,84 @@ class Redis(ScoreCache):
     def hash_cache(cls, cache):
         sorted_tuple = tuple(sorted(cache.items()))
         return sha1(bytes(str(sorted_tuple), 'utf8')).hexdigest()
+
+
+class RedisSentinel(ScoreCache):
+
+    def __init__(self, sentinel, ttl=None, prefix=None, cluster=None, socket_timeout=None):
+        self.sentinel = sentinel
+        self.ttl = int(ttl or TTL)
+        self.prefix = str(prefix or PREFIX)
+        self.cluster = str(cluster or CLUSTER)
+        self.socket_timeout = float(socket_timeout or SOCKET_TIMEOUT)
+
+    def lookup(self, context_name, model_name, rev_id, version=None,
+               injection_cache=None):
+        key = self._generate_key(
+            context_name, model_name, rev_id, version=version,
+            injection_cache=injection_cache)
+
+        replica = self.sentinel.slave_for(self.cluster, socket_timeout=self.socket_timeout)
+        sentinel_logger.debug("Looking up score at {0} in replica".format(key))
+        value = replica.get(key)
+        if value is None:
+            raise KeyError(key)
+        else:
+            return json.loads(str(value, 'utf-8'))
+
+    def store(self, score, context_name, model_name, rev_id, version=None,
+              injection_cache=None):
+        key = self._generate_key(
+            context_name, model_name, rev_id, version=version,
+            injection_cache=injection_cache)
+
+        master = self.sentinel.master_for(self.cluster, socket_timeout=self.socket_timeout)
+        sentinel_logger.debug("Storing score at {0} in master".format(key))
+        master.setex(key, self.ttl, bytes(json.dumps(score), 'utf-8'))
+
+    def _generate_key(self, wiki, model, rev_id, version=None,
+                      injection_cache=None):
+        if injection_cache is None or len(injection_cache) == 0:
+            key_values = [self.prefix, wiki, model, rev_id, version]
+        else:
+            cache_hash = Redis.hash_cache(injection_cache)
+            key_values = [self.prefix, wiki, model, rev_id, version,
+                          cache_hash]
+
+        return ":".join(str(v) for v in key_values)
+
+    @classmethod
+    def from_parameters(cls, hosts, ttl=None, prefix=None, cluster=None,
+                        socket_timeout=None):
+        try:
+            from redis.sentinel import Sentinel
+        except ImportError:
+            raise ImportError("Could not find redis-py.  This packages is " +
+                              "required when using ores.score_caches.RedisSentinel.")
+
+        hosts = [i.split(':') for i in hosts]
+        return cls(Sentinel(hosts, socket_timeout=socket_timeout),
+                   ttl=ttl, prefix=prefix, cluster=cluster,
+                   socket_timeout=socket_timeout)
+
+    @classmethod
+    def from_config(cls, config, name, section_key="score_caches"):
+        """
+        score_caches:
+            redis_sentinel:
+                class: ores.score_caches.RedisSentinel
+                prefix: ores-derp
+                ttl: 9001
+                socket_timeout: 0.1
+                cluster: mymaster
+                hosts:
+                  - localhost:5000
+                  - localhost:5001
+                  - localhost:5002
+        """
+        sentinel_logger.info("Loading RedisSentinel '{0}' from config.".format(name))
+        section = config[section_key][name]
+
+        kwargs = {k: v for k, v in section.items() if k != "class"}
+
+        return cls.from_parameters(**kwargs)
