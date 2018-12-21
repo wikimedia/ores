1deb24cb0bd70ee31e031adcf86bb263a9b14407
Amir Sarabadani <Ladsgroup@gmail.com>
2018-12-21 15:15:46 +0100

    Add mock redis tests for times redis is not available

--- ores/score_caches/tests/test_redis.py
+++ ores/score_caches/tests/test_redis.py
@@ -17,3 +17,30 @@ def cache():
 def test_lookup(cache):
     cache.store(0.421, 'testwiki', 'damaging', 124)
     assert cache.lookup('testwiki', 'damaging', 124) == 0.421
+
+
+class RedisMock():
+    def __init__(self):
+        self.values = {}
+
+    def setex(self, key, ttl, value):
+        self.values[key] = value
+        return True
+
+    def get(self, key):
+        return self.values.get(key)
+
+    def delete(self, key):
+        del self.values[key]
+        return True
+
+
+@pytest.fixture
+def cache_mock():
+    return Redis(RedisMock(), 0, 'foo')
+
+
+@pytest.mark.redis
+def test_lookup_mock(cache_mock):
+    cache_mock.store(0.421, 'testwiki', 'damaging', 124)
+    assert cache_mock.lookup('testwiki', 'damaging', 124) == 0.421
--- ores/task_tracker/tests/test_redis_task_tracker.py
+++ ores/task_tracker/tests/test_redis_task_tracker.py
@@ -29,3 +29,39 @@ def test_release(task_tracker):
     task_tracker.lock('fooo_release', 'value3')
     assert task_tracker.release('fooo_release')
     assert task_tracker.get_in_progress_task('fooo_release') is False
+
+
+class RedisMock():
+    def __init__(self):
+        self.values = {}
+
+    def setex(self, key, ttl, value):
+        self.values[key] = value
+        return True
+
+    def get(self, key):
+        return self.values.get(key)
+
+    def delete(self, key):
+        del self.values[key]
+        return True
+
+
+@pytest.fixture
+def mock_task_tracker():
+    return RedisTaskTracker(RedisMock(), 0, 'foo')
+
+
+def test_lock_mock(mock_task_tracker):
+    assert mock_task_tracker.lock('fooo', 'value1') is True
+
+
+def test_get_in_progress_task_mock(mock_task_tracker):
+    mock_task_tracker.lock('fooo_get', 'value2')
+    assert mock_task_tracker.get_in_progress_task('fooo_get') == 'value2'
+
+
+def test_release_mock(mock_task_tracker):
+    mock_task_tracker.lock('fooo_release', 'value3')
+    assert mock_task_tracker.release('fooo_release')
+    assert mock_task_tracker.get_in_progress_task('fooo_release') is False
