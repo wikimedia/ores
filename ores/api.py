8ef268f6c54b86417d292412a5b5c88b90427849
halfak <aaron.halfaker@gmail.com>
2019-01-11 16:13:31 -0600

    Adds error handling and tests to score_revisions utility

--- ores/api.py
+++ ores/api.py
@@ -9,6 +9,7 @@ Batching and parallelism are set by constructor arguments.
 """
 import logging
 import time
+import traceback
 import urllib.parse
 from concurrent.futures import ThreadPoolExecutor
 
@@ -39,17 +40,22 @@ class Session:
     """
     DEFAULT_USERAGENT = "ores.api default user-agent"
 
-    def __init__(self, host, user_agent=None, batch_size=50,
-                 parallel_requests=4, retries=5):
+    def __init__(self, host, user_agent=None, session=None,
+                 retries=5, batch_size=50, parallel_requests=4):
         self.host = str(host)
+        if session is not None:
+            self._session = session
+        else:
+            self.retries = int(retries)
+
+            self._session = requests.Session()
+            self._session.mount(self.host,
+                                requests.adapters.HTTPAdapter(max_retries=retries))
+
         self.batch_size = int(batch_size)
         self.workers = int(parallel_requests)
-        self.retries = int(retries)
-
-        self._session = requests.Session()
-        self._session.mount(self.host,
-                            requests.adapters.HTTPAdapter(max_retries=retries))
         self.headers = {}
+
         if user_agent is None:
             logger.warning("Sending requests with default User-Agent.  " +
                            "Set 'user_agent' on oresapi.Session to " +
@@ -81,7 +87,7 @@ class Session:
         logging.debug("Starting up thread pool with {0} workers"
                       .format(self.workers))
         with ThreadPoolExecutor(max_workers=self.workers) as executor:
-            futures = []  # A list of future results
+            future_rev_ids = []  # A list of future results and the revids
 
             # This loop loads all rev_id_batch's into the executor for
             # processing
@@ -89,14 +95,23 @@ class Session:
                 rev_id_batch = list(rev_id_batch)
                 logging.debug("Starting batch of {0} revids"
                               .format(len(rev_id_batch)))
-                futures.append(executor.submit(self._score_request,
-                                               context, rev_id_batch,
-                                               models))
+                future_rev_ids.append((
+                    executor.submit(self._score_request, context, rev_id_batch,
+                                    models),
+                    rev_id_batch))
 
             # This loop blocks on reading the futures as soon as they are ready
-            for future in futures:
-                for score in future.result():
-                    yield score
+            for (future, rev_id_batch) in future_rev_ids:
+                try:
+                    for score in future.result():
+                        yield score
+                except RuntimeError as e:
+                    logger.warning(
+                        "An ORES scoring job failed with the following error:")
+                    logger.warning(traceback.format_exc())
+                    for rev_id in rev_id_batch:
+                        yield {m: {"error": e.args[0]}
+                               for m in models}
 
     def _score_request(self, context, rev_ids, models):
         url = self.host + "/v3/scores/{0}/".format(urllib.parse.quote(context))
@@ -107,10 +122,9 @@ class Session:
         logging.debug("Sending score request for {0} revisions"
                       .format(len(rev_ids)))
         start = time.time()
-        response = requests.get(url, params=params,
-                                headers=self.headers,
-                                verify=True)
-
+        response = self._session.get(url, params=params,
+                                     headers=self.headers,
+                                     verify=True, stream=True)
         try:
             doc = response.json()
         except ValueError:
