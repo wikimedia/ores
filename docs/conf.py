a66ca6aeb113857e30d54d8591f90d18b32a7d9a
Andy Craze <acraze@wikimedia.org>
2020-05-11 15:09:34 -0700

    fix linkcheck error

--- docs/conf.py
+++ docs/conf.py
@@ -105,10 +105,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build']
-nitpick_ignore = [
-    ('py:class', 'ores.scoring_systems.CeleryQueue'),
-
-    ]
+# nitpick_ignore = [
+#     ('py:class', 'ores.scoring_systems.CeleryQueue'),
+#
+#     ]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
